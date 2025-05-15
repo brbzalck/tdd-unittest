dd172aef385786e34c9593ef85aeacf28cd3ece1
brbzalck <lukasbo9765@gmail.com>
2025-05-15 09:17:58 -0300

    Asserções (Assertions)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from calculadora import soma
 
-print(soma(2, 2))
-print(soma(20, 10))
-print(soma(2.5, 1.5))
+# print(soma(2, 2))
+# print(soma(20, 10))
+try:
+    print(soma('2.5', 1.5))
+except AssertionError as e:
+    print(f'Conta inválida: {e} ')
+
+print('Conta', soma(25, 25))
